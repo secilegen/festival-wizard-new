ffcadedecbd997d07fe6a1c4ee5cfe502df48710
Philip Butland <Philip_Butland@yahoo.com>
2023-01-30 17:51:00 +0100

    added error and warning messages on the Create Festival page

--- routes/festival.routes.js
+++ routes/festival.routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const mongoose = require('mongoose')
+const { rawListeners } = require('../app')
+const app = require('../app')
 const Festival = require('../models/Festival.model')
 
 router.get('/festivals/create',(req,res)=>{
@@ -8,43 +10,90 @@ router.get('/festivals/create',(req,res)=>{
 
 router.post('/festivals/create',(req,res)=>{
 
+    console.log(req.body)
+
     if (req.body.minPrice && !req.body.maxPrice){
-        console.log("Maximum Price has been set to ", req.body.minPrice)
+
+        message = "Maximum Price has been set to " + req.body.minPrice
+        errorType = "warning"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});
         req.body.maxPrice = req.body.minPrice
+        return;
     }
-    else if (!req.body.minPrice && !req.body.minPrice){
-        console.log("Minimum Price has been set to ", req.body.maxPrice)
+    else if (!req.body.minPrice && req.body.maxPrice){
+        message = "Minimum Price has been set to " + req.body.maxPrice
+        errorType = "warning"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});
         req.body.minPrice = req.body.maxPrice
+        return;
     }
     else if (Number(req.body.minPrice) > Number(req.body.maxPrice)) {
-        console.log("Maximum price must be greater than or equal to minimum price")
-        console.log("Maximum price has been set to ", req.body.minPrice)
+        message = "Maximum Price cannot be lower than Minimum. Max price has been set to " + req.body.minPrice
+        errorType = "warning"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});
         req.body.maxPrice = req.body.minPrice
+        return;
     }
 
     if (req.body.startDate && !req.body.endDate) {
-        console.log("End Date has been set to ", req.body.startDate)
+        message = "End Date has been set to " + req.body.startDate
+        errorType = "warning"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});
         req.body.endDate = req.body.startDate
+        return;
     }
     else if (!req.body.startDate && req.body.endDate) {
-        console.log("Start Date has been set to ", req.body.endDate)
+        message = "Start Date has been set to " + req.body.endDate
+        errorType = "warning"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});
         req.body.startDate = req.body.endDate
+        return;
     }
     else if (req.body.startDate > req.body.endDate) {
-        console.log("Start Date must not be after the End Date")
-        console.log("End Date has been set to ", req.body.startDate)
+        message = "End Date cannot be before the Start Date. End Date has been set to " + req.body.startDate
+        errorType = "warning"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});
         req.body.endDate = req.body.startDate
+        return;
     }
 
+    if (!req.body.name) {
+        message = "You must enter a name for your Festival"
+        errorType="error"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});
+    }
+    else if (!req.body.startDate) {
+        message = "You must enter a Start Date"
+        errorType="error"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});
+    }       
+    else if (!res.body.country) {     // theoretically impossible because of Dropdown menu?
+        message = "You must select a country"
+        errorType="error"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType});    
+    }
+    else if (!res.body.city) {
+        message = "You must enter a city"
+        errorType="error"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType}); 
+    }
+    else if (!res,body.website) {
+        message = "You must enter a website"
+        errorType="error"
+        res.render('festivals/new-festival-form', {errorMessage: message, errorType}); 
+    }
 
-    const {name,imageURL,startDate,endDate,country, city, address,currency,minPrice,maxPrice,website,mustKnow,genre} = req.body
+    else {
+        const {name,imageURL,startDate,endDate,country, city, address,currency,minPrice,maxPrice,website,mustKnow,genre} = req.body
 
-    Festival.create({name:name, imageURL: imageURL, startDate: startDate, endDate:endDate, location: {city:city, country:country, address:address}, currency: currency, minPrice: minPrice,maxPrice: maxPrice,website: website,mustKnow: mustKnow,genre:genre })
-    .then((createdFestival)=>{  
-        res.redirect('/festivals/list')
-        // res.redirect(`/festivals/${createdFestival._id}`)
-    })
-    .catch(err=> console.log('An error occured creating the festival:', err)) 
+        Festival.create({name:name, imageURL: imageURL, startDate: startDate, endDate:endDate, location: {city:city, country:country, address:address}, currency: currency, minPrice: minPrice,maxPrice: maxPrice,website: website,mustKnow: mustKnow,genre:genre })
+        .then((createdFestival)=>{  
+            res.redirect('/festivals/list')
+            // res.redirect(`/festivals/${createdFestival._id}`)
+        })
+        .catch(err=> console.log('An error occured creating the festival:', err)) 
+    }
+
 })
 
 
@@ -92,5 +141,7 @@ router.post('/festivals/:id/edit', (req,res)=>{
     })
     .catch(err=>console.log('Editing error is:', err))
 })
+
+
 
 module.exports = router
